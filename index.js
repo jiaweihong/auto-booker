c701133a921a08472ba86bdc66172f35b648104e
Jia Wei Hong <jiaweihong1@hotmail.com>
2022-06-28 15:59:17 +0800

    Able to log into nottingham gym website

--- index.js
+++ index.js
@@ -1,18 +1,25 @@
-const {Builder, By, Key} = require('selenium-webdriver');
+const {Builder, By, Capabilities} = require('selenium-webdriver');
 const chromedriver = require('chromedriver');
 require('dotenv').config();
 
+
+// problem: takes too long to load page fully then click login, find a way to press log in button as soon as it shows up
+
 async function main() {
+
+    const caps = new Capabilities();
+    caps.setPageLoadStrategy("eager");
+
     try {
-        console.log(process.env.PASSWORD);
-        let driver = await new Builder().forBrowser("chrome").build();
+        let driver = await new Builder().withCapabilities(caps).forBrowser("chrome").build();
     
         // opens up page
         await driver.get("https://www.nottingham.ac.uk/sport/membership/member-login.aspx");
 
         // press login
         await driver.findElement(By.xpath('//*[@id="content"]/div[2]/div[2]/div/a')).click();
-        
+        //console.log(await driver.getCurrentUrl());
+
         // open new tab
         let tabs = await driver.getAllWindowHandles();
 
@@ -20,7 +27,15 @@ async function main() {
         await driver.switchTo().window(tabs[1]);
 
         // input log in details
-        await driver.findElement(By.xpath('//*[@id="username"]')).sendKeys("test");
+        await driver.findElement(By.xpath('//*[@id="username"]')).sendKeys(process.env.USERNAME);
+        await driver.findElement(By.xpath('//*[@id="password"]')).sendKeys(process.env.PASSWORD);
+
+        // click login
+        driver.findElement(By.xpath('/html/body/div[1]/div/div/div[1]/form/div[3]/button')).click();
+
+        
+
+        
     } catch (error) {
         console.log(error);
     }
