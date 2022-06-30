a973cdbcffd136bf3c5fe846252618f5c6f2d2c3
Jia Wei Hong <jiaweihong1@hotmail.com>
2022-06-30 15:26:32 +0800

    Able to select the correct activity slot for a given time and date.

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
-const {Builder, By, Capabilities, until} = require('selenium-webdriver');
+const {Builder, By, Capabilities, until, Key} = require('selenium-webdriver');
 const chromedriver = require('chromedriver');
 const xpaths = require('./xpaths');
+const { listOfActivitiesDiv } = require('./xpaths');
 require('dotenv').config();
 
 async function main() {
@@ -11,12 +12,13 @@ async function main() {
     
     try {
         let driver = await new Builder().withCapabilities(caps).forBrowser("chrome").build();
+        
         // opens up page
         await driver.get("https://www.nottingham.ac.uk/sport/membership/member-login.aspx");
 
         // press login
-        let buttonToOpenLoginPage = returnXpathElement(driver, xpaths.buttonToOpenLoginPage);
-        await buttonToOpenLoginPage.click()
+        let openLoginPageButton = returnXpathElement(driver, xpaths.openLoginPageButton);
+        await openLoginPageButton.click()
 
         // open new tab
         // switch focus of the driver to new tab
@@ -25,24 +27,16 @@ async function main() {
         
         LogIntoBookingWebsite(driver);
         
-        // click make booking button
-        let buttonMakeBooking = returnXpathElement(driver, xpaths.buttonMakeBooking);
-        await buttonMakeBooking.click();
-        
-        let fieldToSelectSportCentre = returnXpathElement(driver, xpaths.fieldToSelectSportCentre);
-        await fieldToSelectSportCentre.click();
 
-        // working fully up till this point
+        let makeBookingButton = returnXpathElement(driver, xpaths.makeBookingButton);
+        await makeBookingButton.click();
 
         // for now, we use statically set the users input.
-        // sports centre: 0 = david ross, 1 = jubilee
-        // category: 0 = sports hall
-
         let sportsCentreChosen = 0;
         // need user input on whether to select david ross or jubilee
 
         if (sportsCentreChosen == 0) {
-            BookActivityDavidRoss();
+            BookActivityDavidRoss(driver, 0, 0);
         } else if (sportsCentreChosen == 1) {
             BookActivityJubileeCampus();
         }
@@ -52,6 +46,7 @@ async function main() {
     }
 }
 
+
 async function LogIntoBookingWebsite(driver){
     let usernameField = returnXpathElement(driver, xpaths.usernameField);
     await usernameField.sendKeys(process.env.USERNAME);
@@ -59,15 +54,15 @@ async function LogIntoBookingWebsite(driver){
     let passwordField = returnXpathElement(driver, xpaths.passwordField);
     await passwordField.sendKeys(process.env.PASSWORD);
 
-    let buttonToLogIntoBookingWebsite = returnXpathElement(driver, xpaths.buttonToLogIntoBookingWebsite)
-    await buttonToLogIntoBookingWebsite.click();
+    let logIntoBookingWebsiteButton = returnXpathElement(driver, xpaths.logIntoBookingWebsiteButton)
+    await logIntoBookingWebsiteButton.click();
 }
 
 function returnXpathElement(driver, xpath) {
     return driver.wait(until.elementLocated(By.xpath(`${xpath}`)), 60000);
 }
 
-async function BookActivityDavidRoss(activityType) {
+async function BookActivityDavidRoss(driver, activityType, categoryType) {
     /*
     activityType
     Volleyball - Hall C/D  = 0
@@ -78,10 +73,24 @@ async function BookActivityDavidRoss(activityType) {
     Basketball Cross Court - Hall C/D (Far) = 5
     Basketball Full Court - Hall C/D = 6
     */
+   
+   try {
+        let selectSportCentreField = returnXpathElement(driver, xpaths.selectSportCentreField);
+        await selectSportCentreField.click();
 
-    try {
+        let davidRossField = returnXpathElement(driver, xpaths.davidRoss);
+        await davidRossField.click();
+
+        if (categoryType == 0 ){
+            let sportsHallRadioButton = returnXpathElement(driver, xpaths.sportsHall);
+            await sportsHallRadioButton.click();
+        }
+
+        // needs to dynamically find the right div containing the correct activity type
+        
         if (activityType == 0 ){
-            await driver.findElement(By.xpath(xpaths.DR_Vball_CD)).click();
+            let volleyball_CD = await driver.wait(until.elementLocated(By.id('booking-activity-option279')));
+            await volleyball_CD.click();
         } else if (activityType == 1){
 
         } else if (activityType == 2){
@@ -95,11 +104,64 @@ async function BookActivityDavidRoss(activityType) {
         } else if (activityType == 6){
             
         }
+        console.log("about clicked view time");
+
+        let viewTimeTableButton = returnXpathElement(driver, xpaths.viewTimetableButton);
+        await viewTimeTableButton.click();
+        console.log("clicked view time");
+
+        bookActivity(driver, "08:00", "01 Jul 2022");
     } catch (error) {
-        
+        console.log(error);
     }
 }
 
+async function bookActivity(driver, timeToBook, date){
+    // get a list of booking slots
+    // for each slot
+    
+    let dateField = await driver.wait(until.elementLocated(By.id("unique-identifier-2")));
+    await dateField.clear();
+    await dateField.sendKeys(date);
+    await dateField.sendKeys(Key.ENTER);
+
+
+
+    // make sure activity slots have been loaded 
+    let toMakeSureActivitySlotsAreLoaded = await driver.wait(until.elementLocated(By.className("col-xs-12 col-sm-6 col-md-6 col-lg-4")));
+
+    // loop through each activitySlot
+    let listOfActivitySlots = await driver.findElements(By.className("col-xs-12 col-sm-6 col-md-6 col-lg-4"));
+    console.log(listOfActivitySlots.length)
+    for (let activitySlot of listOfActivitySlots){
+        let activitySlotAnchor = await activitySlot.findElement(By.css("A"));
+
+        let timeDiv = activitySlotAnchor.findElement(By.css(".timeOfDay"));
+        let timeFromAcitivitySlot = await timeDiv.getText();
+        
+        console.log(timeFromAcitivitySlot);
+
+        if (timeFromAcitivitySlot == timeToBook) {
+
+            await activitySlotAnchor.click();
+            break;
+        } else {
+            continue;
+        }
+    }
+
+    // get child div with class name well-sm activityBox
+    // get child div row
+    // get child div timeofday.innertext
+    // if matches time arg
+    // click on div
+
+
+
+   
+    
+
+}
 
 
 main();
--- xpaths.js
+++ xpaths.js
@@ -1,14 +1,28 @@
 const xpaths = {
-    buttonToOpenLoginPage: '//*[@id="content"]/div[2]/div[2]/div/a',
-    usernameField: '//*[@id="username"]',
-    passwordField: '//*[@id="password"]',
-    buttonToLogIntoBookingWebsite: '/html/body/div/div/div/div[1]/form/div[3]/button',
-    buttonMakeBooking: '//*[@id="main"]/div[2]/div/landing-page-base/div/div/div[2]/landing-page-bookings-base/div/div[2]/account-booking-actions/div/div/div/a',
-    fieldToSelectSportCentre: '//*[@id="bookingPage"]/div[1]/div[1]/div/div[2]/multiple-location-selection/select-two-multiple-children/div/span/span[1]/span/ul/li/input',
-    davidRoss: '//*[@id="select2-o8iz-result-jyrp-479"]/ul/li[1]',
-    JubileeCampus: '//*[@id="select2-o8iz-result-jyrp-479"]/ul/li[2]',
-    SportsHall: '//*[@id="booking-behaviour-option2"]',
-    DR_Vball_CD: '//*[@id="booking-activity-option279"]',
+    openLoginPageButton: '/html/body/form/div[2]/div/div[3]/div/div/div[3]/div[2]/div[3]/div[2]/div[2]/div/a',
+    usernameField: '/html/body/div/div/div/div[1]/form/div[1]/input',
+    passwordField: '/html/body/div/div/div/div[1]/form/div[2]/input',
+    logIntoBookingWebsiteButton: '/html/body/div/div/div/div[1]/form/div[3]/button',
+    makeBookingButton: '/html/body/div[2]/div[2]/div/landing-page-base/div/div/div[2]/landing-page-bookings-base/div/div[2]/account-booking-actions/div/div/div/a',
+    selectSportCentreField: '/html/body/div[2]/div[2]/div/div/div[1]/div[1]/div/div[2]/multiple-location-selection/select-two-multiple-children/div/span/span[1]/span',
+    davidRoss: '/html/body/span[2]/span/span/ul/li[1]/ul/li[1]',
+    jubileeCampus: '/html/body/span[2]/span/span/ul/li[1]/ul/li[2]',
+    sportsHall: '/html/body/div[2]/div[2]/div/div/div[1]/div[1]/booking-behaviour-select/div/div[2]/div[6]/div[1]/input',
+    DR_Vball_CD: '/html/body/div[2]/div[2]/div/div/div[1]/div[2]/booking-activity-select/div/div[2]/div[11]/div[1]/input',
+    viewTimetableButton: '/html/body/div[2]/div[2]/div/div/div[2]/button[1]',
+    timeDivOfBookingSlot: '/html/body/div[2]/div[2]/div/div/sportshall-activity-timetable/div/div[1]/a/div/div/div[1]/div[1]',
+    activityPanelBody: '/html/body/div[2]/div[2]/div/div/div[1]/div[2]/booking-activity-select/div/div[2]',
+    divForChosenActivityButton: '/html/body/div[2]/div[2]/div/div/div[1]/div[2]/booking-activity-select/div/div[2]/div[1]/div[1]',
+    chosenActivityButton: '/html/body/div[2]/div[2]/div/div/div[1]/div[2]/booking-activity-select/div/div[2]/div[1]/div[1]/input',
+
 };
 
 module.exports = xpaths;
+
+//*[@id="select2-301c-result-v6sq-479"]/ul/li[1]
+
+//*[@id="select2-301c-result-v6sq-479"]/ul/li[1]
+
+//*[@id="select2-r8gg-result-vuwr-479"]/ul/li[1]
+
+///html/body/span[2]/span/span/ul/li[1]/ul/li[1]
