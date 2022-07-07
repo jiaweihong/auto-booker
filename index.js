7fb4ac204a50b1857afbe32dc9c652d464a6b1c2
Jia Wei Hong <jiaweihong1@hotmail.com>
2022-07-07 16:56:09 +0800

    Restructure functions and logic of program to mimic human interaction with website
    Also, removes returnXpathElement function, as the program now uses CSS
    selectors to find the appropriate IDS

--- index.js
+++ index.js
@@ -7,55 +7,72 @@ require('dotenv').config();
 const userData = require('./data.json');
 
 async function LogIntoBookingWebsite(driver){
-    let usernameField = returnXpathElement(driver, xpaths.usernameField);
+    let usernameField = await driver.wait(until.elementLocated(By.css('input[id="username"]')), 60000);
     await usernameField.sendKeys(process.env.USERNAME);
 
-    let passwordField = returnXpathElement(driver, xpaths.passwordField);
+    let passwordField = await driver.wait(until.elementLocated(By.css('input[id="password"]')), 60000);
     await passwordField.sendKeys(process.env.PASSWORD);
 
-    let logIntoBookingWebsiteButton = returnXpathElement(driver, xpaths.logIntoBookingWebsiteButton)
+    let logIntoBookingWebsiteButton = await driver.wait(until.elementLocated(By.css('button[name="_eventId_proceed"]')), 60000);
     await logIntoBookingWebsiteButton.click();
 }
 
-function returnXpathElement(driver, xpath) {
-    return driver.wait(until.elementLocated(By.xpath(`${xpath}`)), 60000);
+async function bookActivity(driver, userData) {
+    await selectSportCentreCategoryActivity(driver, userData);
+
+    await buyNowActivity(driver, userData);
 }
 
-async function bookActivityDavidRoss(driver, userData) {
-    await selectClubCategoryActivities(driver, userData);
+async function selectSportCentreCategoryActivity(driver, userData){
+    await selectSportCentre(driver, userData.sportCentreNum);
 
-    let viewTimeTableButton = returnXpathElement(driver, xpaths.viewTimetableButton);
+    // Select category
+    let sportsHallRadio = await driver.wait(until.elementLocated(By.css('input[id="booking-behaviour-option2"]')), 60000);
+    await sportsHallRadio.click();
+
+    await selectActivity(driver, userData.activityNum);
+
+    let viewTimeTableButton = await driver.wait(until.elementLocated(By.css('button[data-test-id="bookings-viewtimetable"]')), 60000);
     await viewTimeTableButton.click();
-
-    await bookActivity(driver, userData);
 }
 
-async function selectClubCategoryActivities(driver, userData){
-    let selectSportCentreField = returnXpathElement(driver, xpaths.selectSportCentreField);
+async function selectSportCentre(driver, sportCentreNum) {
+    let selectSportCentreField = await driver.wait(until.elementLocated(By.css('input[class="select2-search__field"]')), 60000);
     await selectSportCentreField.click();
 
-    let davidRossField = returnXpathElement(driver, xpaths.davidRoss);
-    await davidRossField.click();
-
-    let sportsHallRadioButton = returnXpathElement(driver, xpaths.sportsHall);
-    await sportsHallRadioButton.click();
-    
-    if (userData.activityNum == 279 ){
-        let volleyball_CD = await driver.wait(until.elementLocated(By.id('booking-activity-option279')));
-        await volleyball_CD.click();
-    } 
+    if (sportCentreNum == 18) {
+        let davidRossOption = await driver.wait(until.elementLocated(By.css('li[data-select2-id="18"]')), 60000);
+        await davidRossOption.click();
+    } else if (sportCentreNum == 19) {
+        let jubileeCampusOption = await driver.wait(until.elementLocated(By.css('li[data-select2-id="19"]')), 60000);
+        await jubileeCampusOption.click();
+    } else {
+        throw new Error("No valid sport centre was chosen");
+    }
 }
 
-async function bookActivity(driver, userData){
+async function selectActivity(driver, activityNum){
+    if (activityNum == 279 ){
+        let volleyball_DR_CD = await driver.wait(until.elementLocated(By.id('booking-activity-option279')), 60000);
+        await volleyball_DR_CD.click();
+    } else if (activityNum == 87){
+        let volleyball_JC_H1 = await driver.wait(until.elementLocated(By.id('booking-activity-option87')), 60000);
+        await volleyball_JC_H1.click();
+    } else {
+        throw new Error("No valid activity was chosen");
+    }
+}
+
+async function buyNowActivity(driver, userData){
     await selectDate(driver, userData);
 
     let slot = await getSlot(driver, userData)
 
-    await bookSlot(driver, slot);
+    await buyNowSlot(driver, slot);
 }
 
 async function selectDate(driver, userData){
-    let dateField = await driver.wait(until.elementLocated(By.id("unique-identifier-2")));
+    let dateField = await driver.wait(until.elementLocated(By.id("unique-identifier-2")), 60000);
     await dateField.clear();
     await dateField.sendKeys(userData.activityDate);
     await dateField.sendKeys(Key.ENTER);
@@ -68,7 +85,7 @@ async function selectDate(driver, userData){
 }
 
 async function getSlot(driver, userData) {
-    let listOfSlots = await driver.wait(until.elementsLocated(By.className("col-xs-12 col-sm-6 col-md-6 col-lg-4")));
+    let listOfSlots = await driver.wait(until.elementsLocated(By.className("col-xs-12 col-sm-6 col-md-6 col-lg-4")), 60000);
 
     for (let slot of listOfSlots){
         let slotAnchor = await slot.findElement(By.css("A"));
@@ -95,7 +112,7 @@ async function getSlot(driver, userData) {
     }
 }
 
-async function bookSlot(driver, slot){
+async function buyNowSlot(driver, slot){
     await slot.click();
 
     let button = await driver.wait(until.elementLocated(By.css('button[data-test-id="bookings-sportshall-activitydetails-addandbookanother"]')), 60000);
@@ -118,12 +135,7 @@ async function main() {
 
         await driver.navigate().to("https://universityofnottingham.legendonlineservices.co.uk/enterprise/bookingscentre/index");
 
-        if (userData.sportCentreNum == 30) {
-            await bookActivityDavidRoss(driver, userData);
-        } else if (sportsCentreChosen == 1) {
-            await bookActivityJubileeCampus();
-        }
-        
+        bookActivity(driver, userData);
     } catch (error) {
         console.log(error);
     }
