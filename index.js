414e81ba4acee3b77dc067d8abe91574d9baa06d
Jia Wei Hong <jiaweihong1@hotmail.com>
2022-07-11 17:40:44 +0800

    fix selectSportCentre function
    It can now selects the correct sport centre by getting a list of
    child elements from the unordered list element

--- index.js
+++ index.js
@@ -1,17 +1,18 @@
 const {Builder, By, Capabilities, until, Key} = require('selenium-webdriver');
 const chromedriver = require('chromedriver');
+const chrome = require('selenium-webdriver/chrome');
 require('dotenv').config();
 
 const userData = require('./data.json');
 
 async function LogIntoBookingWebsite(driver){
-    let usernameField = await driver.wait(until.elementLocated(By.css('input[id="username"]')), 60000);
+    let usernameField = await driver.wait(until.elementLocated(By.css('input[id="username"]')), 30000);
     await usernameField.sendKeys(process.env.USERNAME);
 
-    let passwordField = await driver.wait(until.elementLocated(By.css('input[id="password"]')), 60000);
+    let passwordField = await driver.wait(until.elementLocated(By.css('input[id="password"]')), 30000);
     await passwordField.sendKeys(process.env.PASSWORD);
 
-    let logIntoBookingWebsiteButton = await driver.wait(until.elementLocated(By.css('button[name="_eventId_proceed"]')), 60000);
+    let logIntoBookingWebsiteButton = await driver.wait(until.elementLocated(By.css('button[name="_eventId_proceed"]')), 30000);
     await logIntoBookingWebsiteButton.click();
 }
 
@@ -22,28 +23,30 @@ async function bookActivity(driver, userData) {
 }
 
 async function selectSportCentreCategoryActivity(driver, userData){
-    await selectSportCentre(driver, userData.sportCentreNum);
+    await selectSportCentre(driver, userData.sportCentre);
 
     // Select category
-    let sportsHallRadio = await driver.wait(until.elementLocated(By.css('input[id="booking-behaviour-option2"]')), 60000);
+    let sportsHallRadio = await driver.wait(until.elementLocated(By.css('input[id="booking-behaviour-option2"]')), 30000);
     await sportsHallRadio.click();
 
     await selectActivity(driver, userData.activityNum);
 
-    let viewTimeTableButton = await driver.wait(until.elementLocated(By.css('button[data-test-id="bookings-viewtimetable"]')), 60000);
+    let viewTimeTableButton = await driver.wait(until.elementLocated(By.css('button[data-test-id="bookings-viewtimetable"]')), 30000);
     await viewTimeTableButton.click();
 }
 
-async function selectSportCentre(driver, sportCentreNum) {
-    let selectSportCentreField = await driver.wait(until.elementLocated(By.css('input[class="select2-search__field"]')), 60000);
+async function selectSportCentre(driver, sportCentre) {
+    let selectSportCentreField = await driver.wait(until.elementLocated(By.css('input[class="select2-search__field"]')), 30000);
     await selectSportCentreField.click();
 
-    if (sportCentreNum == 18) {
-        let davidRossOption = await driver.wait(until.elementLocated(By.css('li[data-select2-id="18"]')), 60000);
-        await davidRossOption.click();
-    } else if (sportCentreNum == 19) {
-        let jubileeCampusOption = await driver.wait(until.elementLocated(By.css('li[data-select2-id="19"]')), 60000);
-        await jubileeCampusOption.click();
+    let sportCentreUl = await driver.wait(until.elementLocated(By.css('ul[class="select2-results__options select2-results__options--nested"]')), 30000);
+
+    let sportCentreList = await sportCentreUl.findElements(By.css('li'), 30000);
+    
+    if (sportCentre == "David Ross") {
+        await sportCentreList[0].click();
+    } else if (sportCentre == "Jubilee Campus") {
+        await sportCentreList[1].click();
     } else {
         throw new Error("No valid sport centre was chosen");
     }
@@ -51,10 +54,10 @@ async function selectSportCentre(driver, sportCentreNum) {
 
 async function selectActivity(driver, activityNum){
     if (activityNum == 279 ){
-        let volleyball_DR_CD = await driver.wait(until.elementLocated(By.id('booking-activity-option279')), 60000);
+        let volleyball_DR_CD = await driver.wait(until.elementLocated(By.id('booking-activity-option279')), 30000);
         await volleyball_DR_CD.click();
     } else if (activityNum == 87){
-        let volleyball_JC_H1 = await driver.wait(until.elementLocated(By.id('booking-activity-option87')), 60000);
+        let volleyball_JC_H1 = await driver.wait(until.elementLocated(By.id('booking-activity-option87')), 30000);
         await volleyball_JC_H1.click();
     } else {
         throw new Error("No valid activity was chosen");
@@ -70,7 +73,7 @@ async function buyNowActivity(driver, userData){
 }
 
 async function selectDate(driver, userData){
-    let dateField = await driver.wait(until.elementLocated(By.id("unique-identifier-2")), 60000);
+    let dateField = await driver.wait(until.elementLocated(By.id("unique-identifier-2")), 30000);
     await dateField.clear();
     await dateField.sendKeys(userData.activityDate);
     await dateField.sendKeys(Key.ENTER);
@@ -83,7 +86,7 @@ async function selectDate(driver, userData){
 }
 
 async function getSlot(driver, userData) {
-    let listOfSlots = await driver.wait(until.elementsLocated(By.className("col-xs-12 col-sm-6 col-md-6 col-lg-4")), 60000);
+    let listOfSlots = await driver.wait(until.elementsLocated(By.className("col-xs-12 col-sm-6 col-md-6 col-lg-4")), 30000);
 
     for (let slot of listOfSlots){
         let slotAnchor = await slot.findElement(By.css("A"));
@@ -118,12 +121,13 @@ async function getSlot(driver, userData) {
 async function buyNowSlot(driver, slot){
     await slot.click();
 
-    let button = await driver.wait(until.elementLocated(By.css('button[data-test-id="bookings-sportshall-activitydetails-addandbookanother"]')), 60000);
+    let button = await driver.wait(until.elementLocated(By.css('button[data-test-id="bookings-sportshall-activitydetails-addandbookanother"]')), 30000);
     await driver.executeScript("arguments[0].click();", button);
+    console.log("booked");
 }
 
 async function payForBookings(driver){
-    let basketSummaryContinueButton = await driver.wait(until.elementLocated(By.css('button[data-test-id="universalbasket-paymentsummary-continueoptions-continue"]')));
+    let basketSummaryContinueButton = await driver.wait(until.elementLocated(By.css('button[data-test-id="universalbasket-paymentsummary-continueoptions-continue"]')), 30000);
     await basketSummaryContinueButton.click();
 
     await basketSummaryContinueButton.click();
@@ -135,22 +139,31 @@ async function main() {
     // 'eager' means that the get command will be considered complete when the DOM of the page is loaded
     const caps = new Capabilities();
     caps.setPageLoadStrategy("eager");
+
+    const options = new chrome.Options();
+    //options.addArguments('--headless');
+    //options.addArguments("--window-size=1920,1080");
+    //options.addArguments("--disable-gpu");
+    //options.addArguments("--no-sandbox");
     
     try {
-        let driver = await new Builder().withCapabilities(caps).forBrowser("chrome").build();
+        let driver = await new Builder().withCapabilities(caps).forBrowser("chrome").setChromeOptions(options).build();
         
         // opens up page
         await driver.get("https://sso.legendonlineservices.co.uk/sso/nottingham/enterprise");
         
         await LogIntoBookingWebsite(driver);
+        console.log("log");
 
         await driver.navigate().to("https://universityofnottingham.legendonlineservices.co.uk/enterprise/bookingscentre/index");
 
         await bookActivity(driver, userData);
-        //.setChromeOptions(new chrome.Options().addArguments('--headless'))
 
-        await payForBookings(driver);
-        console.log("activity booked");
+        //await driver.navigate().to("https://universityofnottingham.legendonlineservices.co.uk/enterprise/universalbasket/summary");
+
+        //await payForBookings(driver);
+
+        //await driver.quit();
     } catch (error) {
         console.log(error);
     }
