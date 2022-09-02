eec3607b61c3e63befffa42b59829af28b853e05
Jia Wei Hong <jiaweihong1@hotmail.com>
2022-09-02 14:48:24 +0100

    add main page

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,6 @@
 
 import Navbar from './Navbar.js'
-//import MainPage from './MainPage.js'
+import MainPage from './MainPage.js'
 import FaqPage from './FaqPage.js'
 import React from 'react'
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
@@ -13,7 +13,7 @@ const App = () => {
       <Navbar/>
 
       <Routes>
-        
+        <Route path='/' element={<MainPage />}></Route>
         <Route path='/faq' element={<FaqPage />}></Route>
       </Routes>
     </Router>
